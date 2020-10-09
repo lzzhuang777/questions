3a9dd8b8e2ae116011388682a9c091746b5a6226
lzzhuang <2078784331@qq.com>
2020-10-09 17:05:51 +0800

    学习记录2.0

---
+++ questions-common/src/main/java/com/lzz/dto/FailQuestionVO.java
@@ -0,0 +1,15 @@
+package com.lzz.dto;
+
+import lombok.Data;
+
+/**
+ * @author lzz
+ * @version 1.0
+ * @date 2020/10/9 17:04
+ */
+@Data
+public class FailQuestionVO {
+
+   private String typeName;
+    private int count;
+}
